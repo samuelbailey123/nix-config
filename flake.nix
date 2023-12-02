fe4f873c4180666a5d1c8c43421708a5da9109aa
Samuel Bailey <baileysam1997@gmail.com>
2023-12-02 16:47:50 -0600

    Updated nix with some packages

--- flake.nix
+++ flake.nix
@@ -1,21 +1,55 @@
 {
-  description = "Rust Flake";
+  description = "nix-config";
 
+  # Flake inputs
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2305.491812.tar.gz";
   };
 
+  # Flake outputs
   outputs = { self, nixpkgs }:
     let
-      pkgs = nixpkgs.legacyPackages.${nixpkgs.system} or (import nixpkgs {});
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
 
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
     in
     {
-      packages.${nixpkgs.system}.rust = pkgs.rustChannelOf {
-        channel = "stable";
-        date = "nightly";
-      };
-
-      defaultPackage.${nixpkgs.system} = self.packages.${nixpkgs.system}.rust;
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          # The Nix packages provided in the environment
+          packages = with pkgs; [
+            pkgs.bat
+            pkgs.curl
+            pkgs.coreutils
+            pkgs.exa
+            pkgs.git
+            pkgs.gh
+            pkgs.helm
+            pkgs.jq
+            pkgs.kubectl
+            pkgs.k9s
+            pkgs.pgcli
+            pkgs.rclone
+            pkgs.rsync
+            pkgs.watch
+            pkgs.wget
+            pkgs.tmux
+            pkgs.vault
+            pkgs.vim
+            pkgs.telnet
+            pkgs.starship
+          ];
+        };
+      });
     };
 }
