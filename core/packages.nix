7e153d71d53fb890b5ca000b5a944c10b7feb795
Samuel Bailey <samuel.bailey@imubit.com>
2024-01-19 10:48:54 -0600

    Added home manager

--- core/packages.nix
+++ core/packages.nix
@@ -1,8 +1,9 @@
-{ self, pkgs, ... }: {
+{ pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.neofetch
+      environment.systemPackages = with pkgs;
+        [ neofetch
+          
         ];
 
       # Auto upgrade nix package and the daemon service.
@@ -15,9 +16,6 @@
       # Create /etc/zshrc that loads the nix-darwin environment.
       programs.zsh.enable = true;  # default shell on catalina
       # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
 
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
--- flake.nix
+++ flake.nix
@@ -1,17 +1,33 @@
 {
-  description = "Example Darwin system flake";
+  description = "Samuel's Flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#IMUBIT-SAMUELB
     darwinConfigurations."IMUBIT-SAMUELB" = nix-darwin.lib.darwinSystem {
+      modules = [ ./core/packages.nix
+      home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.samuelbailey = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+       ];
+    };
+
+    darwinConfigurations."test" = nix-darwin.lib.darwinSystem {
       modules = [ ./core/packages.nix ];
     };
 
