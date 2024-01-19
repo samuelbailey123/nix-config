8e1c9e9453a03f88b11d9f0337525d6cd67cd133
Samuel Bailey <samuel.bailey@imubit.com>
2024-01-19 10:30:02 -0600

    Seperation of files

--- flake.nix
+++ flake.nix
@@ -8,41 +8,11 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.neofetch
-        ];
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#IMUBIT-SAMUELB
     darwinConfigurations."IMUBIT-SAMUELB" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ ./core/packages.nix ];
     };
 
     # Expose the package set, including overlays, for convenience.
