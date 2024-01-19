52b7898e6ba97ac1258a63448bcbffad208aefec
Samuel Bailey <samuel.bailey@imubit.com>
2024-01-19 10:31:16 -0600

    Added self

--- core/packages.nix
+++ core/packages.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ self, pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
